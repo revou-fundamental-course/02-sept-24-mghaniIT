aeefb6de7694e12e1e727cd6dbcd58a9d7c26644
mghani <ghanioy12346@gmail.com>
2024-09-09 21:23:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const x = 10
+console.log(x)
+
+function hitung() {
+let alas = document.getElementById("alass").value;
+let tinggi = document.getElementById("tinggii").value;
+
+let hasil = perseInt(alas) + perseInt(tinggi)
+console.log(hasil)
+
+document.write('Hasilnya adalah' + hasil)
+
+
+
+// if(jajargenjang.checked)
+
+}
+
+var counter = 0;
+
+console.log(counter)
+
+while(counter < 10 ) {
+    console.log(counter);
+    counter++;
+}
+
